90b1c327bb08ee565600222014942a839ec2874c
jesusGZ <j.gr.zurita@gmail.com>
2022-02-15 16:06:06 -0600

    filtrado de productos

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,85 +1,98 @@
-import styled from "styled-components";
-import Navbar from "../components/Navbar";
-import Announcement from "../components/Announcement";
-import Products from "../components/Products";
-import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
-import { mobile } from "../responsive";
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+import { mobile } from '../responsive';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const Container = styled.div``;
 
 const Title = styled.h1`
-  margin: 20px;
+	margin: 20px;
 `;
 
 const FilterContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const Filter = styled.div`
-  margin: 20px;
-  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+	margin: 20px;
+	${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })}
 `;
 
 const FilterText = styled.span`
-  font-size: 20px;
-  font-weight: 600;
-  margin-right: 20px;
-  ${mobile({ marginRight: "0px" })}
+	font-size: 20px;
+	font-weight: 600;
+	margin-right: 20px;
+	${mobile({ marginRight: '0px' })}
 `;
 
 const Select = styled.select`
-  padding: 10px;
-  margin-right: 20px;
-  ${mobile({ margin: "10px 0px" })}
+	padding: 10px;
+	margin-right: 20px;
+	${mobile({ margin: '10px 0px' })}
 `;
 const Option = styled.option``;
 
 const ProductList = () => {
-  return (
-    <Container>
-      <Navbar />
-      <Announcement />
-      <Title>Vestidos</Title>
-      <FilterContainer>
-        <Filter>
-          <FilterText>Filtrar productos:</FilterText>
-          <Select>
-            <Option disabled selected>
-              Color
-            </Option>
-            <Option>Blanco</Option>
-            <Option>Negro</Option>
-            <Option>Rojo</Option>
-            <Option>Azul</Option>
-            <Option>Amarillo</Option>
-            <Option>Verde</Option>
-          </Select>
-          <Select>
-            <Option disabled selected>
-              Tamaño
-            </Option>
-            <Option>CH</Option>
-            <Option>M</Option>
-            <Option>G</Option>
-            <Option>EG</Option>
-          </Select>
-        </Filter>
-        <Filter>
-          <FilterText>Ordenar productos:</FilterText>
-          <Select>
-            <Option selected>El mas nuevo</Option>
-            <Option>Precio (asc)</Option>
-            <Option>Precio (desc)</Option>
-          </Select>
-        </Filter>
-      </FilterContainer>
-      <Products />
-      <Newsletter />
-      <Footer />
-    </Container>
-  );
+	const location = useLocation();
+
+	const cat = location.pathname.split('/')[2];
+
+	const [filters, setFilters] = useState({});
+	const [sort, setSort] = useState('newest');
+
+	const handleFilters = (e) => {
+		const value = e.target.value;
+		setFilters({
+			...filters,
+			[e.target.name]: value,
+		});
+	};
+
+	return (
+		<Container>
+			<Navbar />
+			<Announcement />
+			<Title>{cat}</Title>
+			<FilterContainer>
+				<Filter>
+					<FilterText>Filtrar productos:</FilterText>
+					<Select name="color" onChange={handleFilters}>
+						<Option disabled>Color</Option>
+						<Option>blanco</Option>
+						<Option>negro</Option>
+						<Option>rojo</Option>
+						<Option>azul</Option>
+						<Option>amarillo</Option>
+						<Option>verde</Option>
+					</Select>
+					<Select name="size" onChange={handleFilters}>
+						<Option disabled>Tamaño</Option>
+						<Option>CH</Option>
+						<Option>M</Option>
+						<Option>G</Option>
+						<Option>EG</Option>
+					</Select>
+				</Filter>
+				<Filter>
+					<FilterText>Ordenar productos:</FilterText>
+					<Select onChange={(e) => setSort(e.target.value)}>
+						<Option value="newest">El mas nuevo</Option>
+						<Option value="asc">Precio (asc)</Option>
+						<Option value="desc">Precio (desc)</Option>
+					</Select>
+				</Filter>
+			</FilterContainer>
+			<Products cat={cat} filters={filters} sort={sort} />
+			<Newsletter />
+			<Footer />
+		</Container>
+	);
 };
 
 export default ProductList;
